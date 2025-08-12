c9716b8767cb72d36ca175f406c8230db0f55de1
jupramirezes <77709508+jupramirezes@users.noreply.github.com>
2025-08-12 00:25:11 -0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,7 @@
 const CACHE = 'control-bar-pwa-v1';
 const ASSETS = ['./','./index.html','./manifest.json','./supabaseClient.js'];
-
+self.addEventListener('install', (e) => { self.skipWaiting(); });
+self.addEventListener('activate', (e) => { e.waitUntil(clients.claim()); });
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
 });
